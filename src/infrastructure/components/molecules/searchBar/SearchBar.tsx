a613600729e6b67ccd2579defed209a204983c61
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-23 19:44:44 -0500

    feat: :lipstick: :zap: molecules

---
+++ src/infrastructure/components/molecules/searchBar/SearchBar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { FC } from "react";
+import { IconProduct, SearchInput, ShortButton } from "../../atoms";
+
+interface SearchBarProps {
+    placeholder?: string;
+    onSearch?: () => void;
+    onInputChange?: (value: string) => void;
+    value?: string;
+}
+
+export const SearchBar: FC<SearchBarProps> = ({
+    placeholder,
+    onSearch,
+    onInputChange,
+    value,
+}) => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                backgroundColor: "#ffd400",
+                padding: "5px 10px",
+                borderRadius: "5px",
+            }}
+        >
+            <IconProduct />
+            <SearchInput
+                placeholder={placeholder}
+                value={value}
+                onChange={(e) => onInputChange && onInputChange(e.target.value)}
+            />
+            <ShortButton onClick={onSearch} />
+        </div>
+    );
+};
