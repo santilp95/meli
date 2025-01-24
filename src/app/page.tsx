faa21395253101b4f26996e0b27ea4d89c31c042
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-23 19:45:32 -0500

    feat: :lipstick: :zap: first view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+'use client';
+import { useState } from "react";
+import { SearchBar } from "@/infrastructure/components";
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = () => {
+    console.log("Searching for:", search);
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Hello world</h1>
+      <SearchBar
+        placeholder="Nunca dejes de buscar"
+        value={search}
+        onInputChange={setSearch}
+        onSearch={handleSearch}
+      />
       </main>
     </div>
   );
