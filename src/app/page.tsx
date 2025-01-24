abfedc36910d977134dbc6343c76265df0284583
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-23 23:34:47 -0500

    refactor: :recycle: :truck: move header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
 'use client';
 
 
-import { SearchBar } from "@/infrastructure/components";
-
 import styles from "./page.module.css";
 
 export default function Home() {
-  const handleSearch = (value: string) => {
-    console.log(value);
-  };
+
 
   return (
     <div className={styles.page}>
-      <header className={styles.header}>
-        <SearchBar
-          placeholder="Nunca dejes de buscar"
-          onSearch={handleSearch}
-        />
-      </header>
       <main className={styles.main}>
         <h1>Hello world</h1>
       </main>
