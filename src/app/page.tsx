4110b2e86815e09a7c3158f8c6b5cc7274538984
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-24 17:24:25 -0500

    style: :lipstick: card use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 
-import { CardProduct } from "@/infrastructure/components";
+import { CardProducts } from "@/infrastructure/components";
+import { sampleProducts } from "@/utils";
+import { IProduct } from "@/domain";
+
 import styles from "./page.module.css";
 
 export default function Home() {
-  const imgProduct =
-    "https://lamanzanamordida.net/app/uploads-lamanzanamordida.net/2024/09/iPhone-16-pro-1.jpg?x=480&y=375&quality=80";
+  const products: IProduct[] = sampleProducts;
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <CardProduct
-          city="Bogotá"
-          detail="Producto de prueba"
-          image={imgProduct}
-          price={1000}
-          hasShippingIcon={true}
-        />
+        <CardProducts products={products} />
       </main>
     </div>
   );
