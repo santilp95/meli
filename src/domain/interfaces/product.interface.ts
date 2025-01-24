788c414062e67411df952f2f409a1f6522bd2f39
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-24 17:05:53 -0500

    feat: :zap:  domain product

---
+++ src/domain/interfaces/product.interface.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: string;
+    image: string;
+    price: number;
+    detail: string;
+    stateOfTheProduct: string;
+    city: string;
+    hasShippingIcon: boolean;
+}
