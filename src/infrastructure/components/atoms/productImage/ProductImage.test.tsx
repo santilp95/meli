27f75cc493271fd8ad033f8e06eabf864d68ff56
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-25 02:12:57 -0500

    refactor:  :recycle: ProductImage

--- src/infrastructure/components/atoms/productImage/ProductImage.test.tsx
+++ src/infrastructure/components/atoms/productImage/ProductImage.test.tsx
@@ -4,14 +4,19 @@ import { ProductImage } from "./ProductImage";
 
 describe("ProductImage", () => {
     const imageUrl = "/path/to/image.jpg";
+    const alt = "Product image";
 
     it("renders the image with the correct src and alt attributes", () => {
-        const { getByAltText } = render(<ProductImage image={imageUrl} />);
+        const { getByAltText } = render(
+            <ProductImage image={imageUrl} alt={alt} />
+        );
         const img = getByAltText("Product image");
 
         expect(img).toBeInTheDocument();
-        expect(img).toHaveAttribute("src", "/_next/image?url=%2Fpath%2Fto%2Fimage.jpg&w=1920&q=75");
+        expect(img).toHaveAttribute(
+            "src",
+            "/_next/image?url=%2Fpath%2Fto%2Fimage.jpg&w=1920&q=75"
+        );
         expect(img).toHaveAttribute("alt", "Product image");
     });
-
 });
--- src/infrastructure/components/atoms/productImage/ProductImage.tsx
+++ src/infrastructure/components/atoms/productImage/ProductImage.tsx
@@ -8,16 +8,20 @@ interface ProductImageProps {
      * Image source
      */
     image: string;
+    /**
+     * Image alt
+     */
+    alt: string;
 }
 
 /**
  * Component to render the main product image
  */
-export const ProductImage: FC<ProductImageProps> = ({ image }) => {
+export const ProductImage: FC<ProductImageProps> = ({ image , alt}) => {
     return (
         <Image
             src={image}
-            alt="Product image"
+            alt={alt}
             width={680}
             height={680}
             style={{
