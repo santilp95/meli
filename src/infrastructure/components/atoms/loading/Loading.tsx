7df402d067ca1c8cc5ae3427b13902f816199aa6
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-27 00:58:58 -0500

    style: :lipstick: Loading

--- src/infrastructure/components/atoms/loading/Loading.tsx
+++ src/infrastructure/components/atoms/loading/Loading.tsx
@@ -1,6 +1,8 @@
+import { FC } from 'react';
+
 import './loading.css';
 
-export const Loading: React.FC = () => {
+export const Loading: FC = () => {
     return (
         <div className="loading-overlay" data-testid="loading-overlay">
             <div className="spinner" data-testid="loading-spinner"></div>
