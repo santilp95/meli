537c7939d6fa44f805c79e80419195a816309e33
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-26 23:27:37 -0500

    feat: :bug: prettier msByID

--- src/infrastructure/services/msByID/msByID.test.ts
+++ src/infrastructure/services/msByID/msByID.test.ts
@@ -1,17 +1,15 @@
-import { msById } from './msByID';
-import { FetchAdapter } from '../../../application/utils/fetch/fetchAdapter';
+import { msById } from "./msByID";
+import { FetchAdapter } from "../../../application/utils/fetch/fetchAdapter";
 
-
-
-describe('msById', () => {
-    const id = 'test';
+describe("msById", () => {
+    const id = "test";
 
     const mockResponse = {
-        id: 'test',
-        title: 'Product 1',
+        id: "test",
+        title: "Product 1",
         price: 100,
-        thumbnail: 'http://example.com/image1.jpg',
-        condition: 'new',
+        thumbnail: "http://example.com/image1.jpg",
+        condition: "new",
         shipping: {
             free_shipping: true,
         },
@@ -21,18 +19,18 @@ describe('msById', () => {
         jest.clearAllMocks();
     });
 
-    it('should fetch data and get the id result', async () => {
-
-        jest.spyOn(FetchAdapter.prototype, 'get').mockResolvedValue(mockResponse)
+    it("should fetch data and get the id result", async () => {
+        jest.spyOn(FetchAdapter.prototype, "get").mockResolvedValue(mockResponse);
 
         const result = await msById(id);
         expect(result).toEqual(mockResponse);
     });
 
+    it("should throw an error if the fetch fails", async () => {
+        jest
+            .spyOn(FetchAdapter.prototype, "get")
+            .mockRejectedValue(new Error("test error"));
 
-    it('should throw an error if the fetch fails', async () => {
-        jest.spyOn(FetchAdapter.prototype, 'get').mockRejectedValue(new Error('test error'));
-
-        await expect(msById(id)).rejects.toThrow('test error');
+        await expect(msById(id)).rejects.toThrow("test error");
     });
 });
