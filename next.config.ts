38b936cf6371277e21d35b1ef49c5e7c9ca8a85b
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-26 21:12:36 -0500

    feat: :zap: :construction: call apy query :alien:

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images:{
-    domains: ['lamanzanamordida.net'],
+    domains: ['lamanzanamordida.net','http2.mlstatic.com'],
   }
   /* config options here */
 };
--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,14 +1,46 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import { CardProducts } from "@/infrastructure/components";
-import { sampleProducts } from "@/utils";
-import { IProduct } from "@/domain";
+import { msQuery } from "@/infrastructure/services/msQuery/msQuery";
+import { IProduct, IResponseTransformQuery } from "@/domain";
+
 
 export default function SearchResultPage() {
-    const products: IProduct[] = sampleProducts;
+    const [products, setProducts] = useState<IProduct[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const search = searchParams.get("search");
+
+    useEffect(() => {
+        if (search) {
+            setLoading(true);
+            msQuery(search)
+                .then((response: IResponseTransformQuery) => {
+                    const products: IProduct[] = response.items.map((item)=>({
+                        id: item.id,
+                        image: item.picture,
+                        price: item.price.amount,
+                        detail: item.title,
+                        stateOfTheProduct: item.condition,
+                        city: 'Bogotá',
+                        hasShippingIcon: item.free_shipping,
+                    }))
+                    console.log(response);
+                    setProducts(products);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    setError(error.message);
+                    setLoading(false);
+                });
+        }
+    }, [search]);
+
     const handleProductClick = (id: string) => {
         router.push(`/items/${id}`);
     };
@@ -16,7 +48,11 @@ export default function SearchResultPage() {
     return (
         <div className="page">
             <main className="main">
-                <CardProducts products={products} onProductClick={handleProductClick} />
+                {loading && <p>Loading...</p>}
+                {error && <p>Error: {error}</p>}
+                {!loading && !error && (
+                    <CardProducts products={products} onProductClick={handleProductClick} />
+                )}
             </main>
         </div>
     );
