32dd7f20987ae63427fff65d8e8c4e301a48d737
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-26 23:33:07 -0500

    feat: :bug: prettier msByID

--- src/infrastructure/services/msByID/msByID.ts
+++ src/infrastructure/services/msByID/msByID.ts
@@ -1,21 +1,19 @@
-
 import { FetchAdapter } from "@/application";
 import { IResponseDataByID } from "@/domain";
 
-
-export const msById = async(id:string): Promise<IResponseDataByID> =>{
+export const msById = async (id: string): Promise<IResponseDataByID> => {
     const fetchAdapter = new FetchAdapter();
-        const baseuRL = "https://api.mercadolibre.com";
-        const url = `${baseuRL}/items/${id}`;
-        const headers = { "Content-Type": "application/json" };
+    const baseuRL = "https://api.mercadolibre.com";
+    const url = `${baseuRL}/items/${id}`;
+    const headers = { "Content-Type": "application/json" };
 
-        try {
-            const result = await fetchAdapter.get<IResponseDataByID, typeof headers>({
-                url,
-                headers,
-            });
-            return result;
-        } catch (error) {
-            throw new Error(`[Error] in msById - ${error}`);
-        }
-}
+    try {
+        const result = await fetchAdapter.get<IResponseDataByID, typeof headers>({
+            url,
+            headers,
+        });
+        return result;
+    } catch (error) {
+        throw new Error(`[Error] in msById - ${error}`);
+    }
+};
