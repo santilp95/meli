9de41daf2a75f0111fc02865164000d3e17521b8
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2025-01-27 15:14:08 -0500

    test: :recycle: :tes_tube: new functionalitty

--- src/infrastructure/components/atoms/iconProduct/IconProduct.test.tsx
+++ src/infrastructure/components/atoms/iconProduct/IconProduct.test.tsx
@@ -1,5 +1,4 @@
-import {render} from  '@testing-library/react';
-
+import { render, fireEvent } from '@testing-library/react';
 import { IconProduct } from './IconProduct';
 
 describe('IconProduct', () => {
@@ -15,5 +14,13 @@ describe('IconProduct', () => {
     expect(logoImage).toHaveAttribute('src', '/_next/image?url=%2Fassets%2FLogo_ML.png&w=96&q=75');
     expect(logoImage).toHaveAttribute('width', '40');
     expect(logoImage).toHaveAttribute('height', '30');
+  });
+
+  it('calls onClick when clicked', () => {
+    const handleClick = jest.fn();
+    const { getByRole } = render(<IconProduct onClick={handleClick} />);
+    const button = getByRole('button');
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
--- src/infrastructure/components/atoms/iconProduct/IconProduct.tsx
+++ src/infrastructure/components/atoms/iconProduct/IconProduct.tsx
@@ -1,16 +1,31 @@
 "use client";
-import { FC } from 'react'
-import Image from 'next/image';
+import { FC } from "react";
+import Image from "next/image";
 
 const logo = "/assets/Logo_ML.png";
+
+interface IconProductProps {
+  /**
+   * Function to handle the click event
+   */
+  onClick?: () => void;
+}
 
 /**
  * Icon product component
  */
-export const IconProduct:FC = () => {
+export const IconProduct: FC<IconProductProps> = ({ onClick }) => {
   return (
-    <div>
+    <button
+      onClick={onClick}
+      style={{
+        border: "none",
+        background: "none",
+        padding: 0,
+        cursor: "pointer",
+      }}
+    >
       <Image src={logo} alt="Logo" width={40} height={30} />
-    </div>
-  )
-}
+    </button>
+  );
+};
